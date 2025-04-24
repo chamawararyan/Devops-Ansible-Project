dad97414fb86146ca1198a3bdc351130ff1b105b
Aryan <109079436+chamawararyan@users.noreply.github.com>
2025-04-24 23:49:25 +0530

    Project-Infra.tf

--- Project-Infra.tf
+++ Project-Infra.tf
@@ -1,20 +1,37 @@
-provider "aws"{
-region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
 }
 
-variable "instance_names"{
-default = ["jenkins", "Monitoring-Server"]
+# First group of instances
+variable "instance_names_one" {
+  default = ["jenkins", "Monitoring-Server"]
 }
 
-resource "aws_instance" "infra" {
-count = length(var.instance_names)
-ami = "ami-080b1a55a0ad28c02"
-instance_type = "t2.medium"
-key_name = "devops"
-vpc_security_group_ids = ["sg-0436b0616cc453e15"]
+resource "aws_instance" "infra_1" {
+  count         = length(var.instance_names_one)
+  ami           = "ami-080b1a55a0ad28c02"
+  instance_type = "t2.medium"
+  key_name      = "devops"
+  vpc_security_group_ids = ["sg-0436b0616cc453e15"]
 
-tags = {
-Name = var.instance_names[count.index]
-}
+  tags = {
+    Name = var.instance_names_one[count.index]
+  }
 }
 
+# Second group of instances
+variable "instance_names_two" {
+  default = ["ansible", "dev-1", "dev-2", "test-1", "test-2"]
+}
+
+resource "aws_instance" "infra_2" {
+  count         = length(var.instance_names_two)
+  ami           = "ami-080b1a55a0ad28c02"
+  instance_type = "t2.micro"
+  key_name      = "devops"
+  vpc_security_group_ids = ["sg-0436b0616cc453e15"]
+
+  tags = {
+    Name = var.instance_names_two[count.index]
+  }
+}
